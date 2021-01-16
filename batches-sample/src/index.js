10770a49dceea69e19c8eb430328433deb77762b
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-01-16 17:40:53 -0600

    greeting sample

---
+++ batches-sample/src/index.js
@@ -0,0 +1,8 @@
+import react from 'react'
+import ReactDOM from 'react-dom'
+
+const greeting =  <h1> hey there </h1>
+
+const divRoot = document.querySelector('#root')
+
+ReactDOM.render(greeting, divRoot );
