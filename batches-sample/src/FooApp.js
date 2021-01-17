73656244938dfa46e1cd5c317de4b08a19972736
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-01-16 18:31:45 -0600

    [update]: refactor

--- batches-sample/src/FooApp.js
+++ batches-sample/src/FooApp.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const FooApp = () => {
-    return <h1>it's works</h1>
+    return (
+        <>
+            <h1>it's works</h1>
+            <p>foo, bar</p>   
+        </>
+    );
 }
 
 export default FooApp;
